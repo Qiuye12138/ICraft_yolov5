1e6b7f3d537e13c20d7d47233331390c2aff8a09
xiao tang <tangxiao@fmsh.com.cn>
2022-08-24 14:16:05 +0800

    🐛 fix: fix the bug during export

--- models/yolo.py
+++ models/yolo.py
@@ -57,9 +57,9 @@ class Detect(nn.Module):
         z = []  # inference output
         for i in range(self.nl):
             x[i] = self.m[i](x[i]) * RATIO[i]  # conv
-            bs, _, ny, nx = x[i].shape  # x(bs,255,20,20) to x(bs,3,20,20,85)
-            x[i] = x[i].view(bs, self.na, self.no, ny, nx).permute(0, 1, 3, 4, 2).contiguous()
             if self.export == False:
+                bs, _, ny, nx = x[i].shape  # x(bs,255,20,20) to x(bs,3,20,20,85)
+                x[i] = x[i].view(bs, self.na, self.no, ny, nx).permute(0, 1, 3, 4, 2).contiguous()
                 if not self.training:  # inference
                     if self.onnx_dynamic or self.grid[i].shape[2:4] != x[i].shape[2:4]:
                         self.grid[i], self.anchor_grid[i] = self._make_grid(nx, ny, i)
